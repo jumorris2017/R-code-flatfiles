4b04620635939bb9eb3a605353c2eb28fa29f50c
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2018-03-05 17:07:50 -0800

    3.5.18

--- 2018-02-26_barista-shift_turnover_projections.R
+++ 2018-02-26_barista-shift_turnover_projections.R
@@ -47,10 +47,33 @@ write.csv(lyhctermq3,file="O:/CoOp/CoOp194_PROReportng&OM/Julie/barista-shift_te
 ##DT2 - take proportion topline terms for each store, and back out from DT1 preds
 #DT1
 #set up data as time series
-tempDT1 <- temp[FY==2017|(FY==2018&FP<5), list(barista.term = sum(barista.term,na.rm=T)), by=c("FP","FY")]
-tempDT1ts <- ts(tempDT1, start = c(2017, 1), frequency = 12)
+tempDT1 <- temp[(FY>=2013&FY<=2017)|(FY==2018&FP<5), list(barista.term = sum(barista.term,na.rm=T)), by=c("FP","FY")]
+tempDT1ts <- ts(tempDT1, start = c(2013, 1), frequency = 12)
 fit <- auto.arima(tempDT1ts[,"barista.term"])
-fc <- forecast(fit, h=4)
-# newdata <- as.data.table(expand.grid(FP = c(6:9), FY = 2018, barista.term = NA))
+fc <- forecast(fit, h=5)
+predvalues <- fc$mean[1:5]
+plot(forecast(fit,h=5),main="Forecasted Topline Barista Terms")
+#expand grid and pull back
+newdata <- as.data.table(expand.grid(FP = c(5:9), FY = 2018, barista.term = NA))
+newdata[, barista.term := predvalues]
+#rbind predictions to original agged dataset
+l = list(tempDT1,newdata)
+tempDT1 <- rbindlist(l,use.names=T,fill=T)
+setnames(tempDT1,"barista.term","bar_term_agg")
+
+#pull monthly agg into store-level dataset
+stmonthprp <- left_join(temp,tempDT1,by=c("FP","FY"))
+setDT(stmonthprp)
+#only needed for last year's months of interest
+stmonthprp <- stmonthprp[FY==2017&(FP>=7&FP<=9)]
+stmonthprp[barista.term==0, bar_term_prp := 0]
+stmonthprp[barista.term>0, bar_term_prp := barista.term/bar_term_agg]
+#only keep month and proportions
+stmonthprp <- stmonthprp[, .(StoreNum,FP,bar_term_prp)]
+tempDT1 <- tempDT1[FY==2018&(FP>=7&FP<=9)]
+stmonthpred <- left_join(stmonthprp,tempDT1,by=c("FP"))
+setDT(stmonthpred)
+stmonthpred[, barista.term := round(bar_term_prp*bar_term_agg,0)]
+
 # fit <- tslm(barista.term ~ FP + FY + StoreNum, data=tempDT1ts)
 # fc <- forecast(fit, h=4, newdata = newdata)
