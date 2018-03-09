1d299f1dcca0d2e59906b407a28dd7b45337cef5
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2018-03-08 16:26:18 -0800

    3.8.18

--- 2018-02-26_barista-shift_turnover_projections.R
+++ 2018-02-26_barista-shift_turnover_projections.R
@@ -5,7 +5,9 @@
 #load libraries
 library(data.table)
 library(forecast)
+library(plm)
 library(utils)
+set.seed(98115)
 
 #load data (from Megan)
 hc <- fread("O:/CoOp/CoOp194_PROReportng&OM/Megan/StoreRisk/EverythingEverJan.csv")
@@ -39,13 +41,13 @@ lyhctermq3 <- lyhcterm[(FY==2017&FP>=7&FP<=9)]
 lyhctermq3 <- lyhctermq3[, .(StoreNum,FP,barista.term,shift.term)]
 
 #write.csv
-write.csv(lyhctermq3,file="O:/CoOp/CoOp194_PROReportng&OM/Julie/barista-shift_terms_Q3FY18.csv")
-
+# write.csv(lyhctermq3,file="O:/CoOp/CoOp194_PROReportng&OM/Julie/barista-shift_terms_Q3FY18.csv")
 
 ##alternative approach
 ##DT1 - agg at topline - predict out 4 periods
 ##DT2 - take proportion topline terms for each store, and back out from DT1 preds
-#DT1
+
+#barista
 #set up data as time series
 tempDT1 <- temp[(FY>=2013&FY<=2017)|(FY==2018&FP<5), list(barista.term = sum(barista.term,na.rm=T)), by=c("FP","FY")]
 tempDT1ts <- ts(tempDT1, start = c(2013, 1), frequency = 12)
@@ -75,5 +77,32 @@ stmonthpred <- left_join(stmonthprp,tempDT1,by=c("FP"))
 setDT(stmonthpred)
 stmonthpred[, barista.term := round(bar_term_prp*bar_term_agg,0)]
 
-# fit <- tslm(barista.term ~ FP + FY + StoreNum, data=tempDT1ts)
-# fc <- forecast(fit, h=4, newdata = newdata)
+#shift
+#set up data as time series
+tempDT1 <- temp[(FY>=2013&FY<=2017)|(FY==2018&FP<5), list(shift.term = sum(shift.term,na.rm=T)), by=c("FP","FY")]
+tempDT1ts <- ts(tempDT1, start = c(2013, 1), frequency = 12)
+fit <- auto.arima(tempDT1ts[,"shift.term"])
+fc <- forecast(fit, h=5)
+predvalues <- fc$mean[1:5]
+plot(forecast(fit,h=5),main="Forecasted Topline Shift Terms")
+#expand grid and pull back
+newdata <- as.data.table(expand.grid(FP = c(5:9), FY = 2018, shift.term = NA))
+newdata[, shift.term := predvalues]
+#rbind predictions to original agged dataset
+l = list(tempDT1,newdata)
+tempDT1 <- rbindlist(l,use.names=T,fill=T)
+setnames(tempDT1,"shift.term","ss_term_agg")
+
+#pull monthly agg into store-level dataset
+stmonthprp <- left_join(temp,tempDT1,by=c("FP","FY"))
+setDT(stmonthprp)
+#only needed for last year's months of interest
+stmonthprp <- stmonthprp[FY==2017&(FP>=7&FP<=9)]
+stmonthprp[shift.term==0, ss_term_prp := 0]
+stmonthprp[shift.term>0, ss_term_prp := shift.term/ss_term_agg]
+#only keep month and proportions
+stmonthprp <- stmonthprp[, .(StoreNum,FP,ss_term_prp)]
+tempDT1 <- tempDT1[FY==2018&(FP>=7&FP<=9)]
+stmonthpred <- left_join(stmonthprp,tempDT1,by=c("FP"))
+setDT(stmonthpred)
+stmonthpred[, shift.term := round(ss_term_prp*ss_term_agg,0)]
